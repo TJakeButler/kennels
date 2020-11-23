fd92c60eff32ec8a31596905a5b472b8a6101338
Jake Butler <tjakebutler@gmail.com>
2020-11-23 15:09:18 -0600

    created customer, employee, and locations modules. They are now all rendering to the DOM

---
+++ src/components/customer/Customer.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Customer.css"
+
+export const Customer = () => (
+    <section className="customer">
+        <h3 className="customer__name">Brenda</h3>
+        <div className="customer__breed">She's a loyal customer</div>
+    </section>
+)
---
+++ src/components/employee/Employee.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employee.css"
+
+export const Employee = () => (
+    <section className="employee">
+        <h3 className="employee__name">Jake</h3>
+        <div className="employee__breed">The Boss</div>
+    </section>
+)
