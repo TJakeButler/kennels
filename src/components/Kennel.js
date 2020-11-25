6924fdbb0dcb5d9773b1648013b24e6f78efee38
Jake Butler <tjakebutler@gmail.com>
2020-11-25 10:41:02 -0600

    created Nav Bar with routing functionality. Replaced Kennel.js with Nav Bar rathe than invoking modules insideof Kennel

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,47 +1,12 @@
 import React from "react"
-import { LocationList } from "./location/LocationList"
-import { LocationProvider } from "./location/LocationProvider"
-import { EmployeeList } from "./employee/EmployeeList"
-import { EmployeeProvider } from "./employee/EmployeeProvider"
-import { CustomerList } from "./customer/CustomerList"
-import { CustomerProvider } from "./customer/CustomerProvider"
-import { AnimalList } from "./animal/AnimalList"
-import { AnimalProvider } from "./animal/AnimalProvider"
-import "./employee/Employee.css"
-import "./customer/Customer.css"
-import "./location/Location.css"
-
+import { Route } from "react-router-dom"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
+import "./Kennel.css"
 
 export const Kennel = () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
-        <h2>Animals</h2>
-        <AnimalProvider>
-            <AnimalList />
-        </AnimalProvider>
-
-        <h2>Employees</h2>
-        <EmployeeProvider>
-            <EmployeeList />
-        </EmployeeProvider>
-       
-
-        <h2>Customers</h2>
-        <CustomerProvider>
-            <CustomerList />
-        </CustomerProvider>
-        
-
-        <h2>Locations</h2>
-        <LocationProvider>
-            <LocationList />
-        </LocationProvider>
+        <NavBar />
+        <ApplicationViews />
     </>
 )
