02d8e99ba038326ef1d0f7046b1d1d1bb3ece86c
Jake Butler <tjakebutler@gmail.com>
2020-11-30 13:36:36 -0600

    worked through chapter 8, we added an employeeFrom component. This allows us to create a new employee by clicking a button that renders to the DOM when we navigate to the Employees end point. This button presents the user with a form and they are able to submit a new employee which is posted to the Database and re-rendered to the DOM.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -7,18 +7,55 @@ import { AnimalList } from "./animal/AnimalList"
 import { CustomerList } from "./customer/CustomerList"
 import { CustomerProvider } from "./customer/CustomerProvider"
 import { EmployeeList } from "./employee/EmployeeList"
-import { EmployeeProvider } from "./employee/EmployeeProvider"
+import { EmployeeContext, EmployeeProvider } from "./employee/EmployeeProvider"
+// chapter 8
+import { EmployeeForm } from "./employee/EmployeeForm"
 
 export const ApplicationViews = (props) => {
-    return (
+    return (<>
         <AnimalProvider>
+            <LocationProvider>
+                <CustomerProvider>
+                    <Route path="/animals">
+                        <AnimalList />
+                    </Route>
+                </CustomerProvider>
+            </LocationProvider>
+        </AnimalProvider>
+
+        <AnimalProvider>
+            <LocationProvider>
+                <CustomerProvider>
+                    <Route path="/customers">
+                        <CustomerList />
+                    </Route>
+                </CustomerProvider>
+            </LocationProvider>
+        </AnimalProvider>
+
+        <AnimalProvider>
+                <LocationProvider>
+            <EmployeeProvider>
+                    <Route exact path="/employees" render={
+                        props => <EmployeeList {...props} />
+                    } />
+
+                    <Route exact path="/employees/create" render={
+                        props => <EmployeeForm {...props} />
+                    } />
+            </EmployeeProvider>
+                </LocationProvider>
+        </AnimalProvider>
+
+
+
         <LocationProvider>
-            <CustomerProvider>
-                <Route exact path="/animals">
-                    <AnimalList />
-                </Route>
-            </CustomerProvider>
+            {/* Render the location list when http://localhost:3000/ */}
+            <Route exact path="/">
+                <LocationList />
+            </Route>
         </LocationProvider>
-    </AnimalProvider>
+
+    </>
     )
 }
